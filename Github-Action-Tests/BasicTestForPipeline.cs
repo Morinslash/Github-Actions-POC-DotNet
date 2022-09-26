e9842eb2d25ee944941ab460713e2b04b79b3495
Sebastian Kulinski <sebastian.elf.kulinski@gmail.com>
2022-09-26 09:54:42 +0200

    Provide one fake tests and publishing tests results

---
+++ Github-Action-Tests/BasicTestForPipeline.cs
@@ -0,0 +1,10 @@
+namespace Github_Action_Tests;
+
+public class BasicTestForPipeline
+{
+    [Fact]
+    public void ShouldBeTrue()
+    {
+        Assert.True(true);
+    }
+}
--- Github-Action-Tests/UnitTest1.cs
+++
@@ -1,9 +0,0 @@
-namespace Github_Action_Tests;
-
-public class UnitTest1
-{
-    [Fact]
-    public void Test1()
-    {
-    }
-}
